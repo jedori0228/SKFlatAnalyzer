6d85ee2cf4950a56c1d9df6553d463ee58faaee3
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-10-07 18:20:15 +0900

    JetPlot added

--- src/HNWRAnalyzer.C
+++ src/HNWRAnalyzer.C
@@ -322,6 +322,7 @@ void HNWRAnalyzer::executeEventFromParameter(AnalyzerParameter param){
         }
 
         FillLeptonPlots(leps, this_region, weight);
+        FillJetPlots(jets, fatjets, this_region, weight);
 
       } // END if(pass Region)
 
