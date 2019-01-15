ccd3a27451ea5d870ee28f8b0e5c9ac5765491da
Jae Sung Kim <jae.sung.kim@cern.ch>
2019-01-16 03:32:34 +0900

    update HNWRSignalStudy

--- Analyzers/src/HNWRSignalStudy.C
+++ Analyzers/src/HNWRSignalStudy.C
@@ -19,9 +19,122 @@ void HNWRSignalStudy::executeEventFromParameter(AnalyzerParameter param){
   FillHist("NoCut", 0., 1., 1, 0., 1.);
   vector<Gen> gens = GetGens();
 
-/*
- //=== 190113_ElectronTriggerTest
+  //==== Find Gen objects
+  Gen gen_WR, gen_N, gen_priLep, gen_secLep, gen_jet1, gen_jet2;
+  int gen_N_PID = -1;
+  bool gen_SignalLeptonChannel = -1; // 0 : ee, 1 : mm
+  bool GenAllFound = true;
+  bool IsOffShellWR = false;
 
+  for(unsigned int i=2; i<gens.size(); i++){
+    Gen gen = gens.at(i);
+    if(abs( gen.PID() ) == 9900024 || abs( gen.PID() ) == 34){
+
+     //=== pythia sample has strange things. first WR is nan
+     double forchecknan = gen.M();
+     if(forchecknan != forchecknan) continue;
+
+      gen_WR = gen;
+      break;
+    }
+  }
+  if(gen_WR.IsEmpty()){
+    IsOffShellWR = true;
+    cout << "Can't file gen_WR" << endl;
+    PrintGen(gens);
+    FillHist("GENFIND_no_gen_WR", 0., 1., 1, 0., 1.);
+    GenAllFound = false;
+  }
+  FillHist("M_gen_WR", gen_WR.M(), 1., 6000, 0., 6000.);
+
+  for(unsigned int i=2; i<gens.size(); i++){
+    Gen gen = gens.at(i);
+    if(abs( gen.PID() ) == 9900012 || abs( gen.PID() ) == 9900014 ){
+      gen_N_PID = gen.PID();
+      gen_N = gen;
+      if(abs( gen.PID() ) == 9900012) gen_SignalLeptonChannel = 0;
+      else if(abs( gen.PID() ) == 9900014) gen_SignalLeptonChannel = 1;
+      else gen_SignalLeptonChannel = 999;
+      break;
+    }
+  }
+  if(gen_N.IsEmpty()){
+    cout << "Can't file gen_N" << endl;
+    PrintGen(gens);
+    FillHist("GENFIND_no_gen_N", 0., 1., 1, 0., 1.);
+    GenAllFound = false;
+  }
+  FillHist("M_gen_N", gen_N.M(), 1., 6000, 0., 6000.);
+  FillHist("gen_SignalLeptonChannel", gen_SignalLeptonChannel, 1., 2, 0., 2.);
+
+
+  if(!IsOffShellWR){
+    for(unsigned int i=2; i<gens.size(); i++){
+      Gen gen = gens.at(i);
+      Gen mother = gens.at(gen.MotherIndex());
+      if(abs( mother.PID() ) == 9900024 || abs( mother.PID() ) == 34 ){
+        if(abs( gen.PID() ) == 11 || abs( gen.PID() ) == 13){
+          gen_priLep = gen;
+          break;
+        }
+      }
+    }
+  }
+  else{
+    for(unsigned int i=2; i<gens.size(); i++){
+      Gen gen = gens.at(i);
+      Gen mother = gens.at(gen.MotherIndex());
+      if(abs( gen.PID() ) == 11 || abs( gen.PID() ) == 13){
+        gen_priLep = gen;
+        break;
+      }
+    }
+  }
+  if(gen_priLep.IsEmpty()){
+    cout << "Can't file gen_priLep" << endl;
+    PrintGen(gens);
+    FillHist("GENFIND_no_gen_priLep", 0., 1., 1, 0., 1.);
+    GenAllFound = false;
+  }
+
+  for(unsigned int i=2; i<gens.size(); i++){
+    Gen gen = gens.at(i);
+    Gen mother = gens.at(gen.MotherIndex());
+    if(abs( mother.PID() ) == abs( gen_N_PID )){
+      if(abs( gen.PID() ) == 11 || abs( gen.PID() ) == 13){
+        gen_secLep = gen;
+        break;
+      }
+    }
+  }
+  if(gen_secLep.IsEmpty()){
+    cout << "Can't file gen_secLep" << endl;
+    PrintGen(gens);
+    FillHist("GENFIND_no_gen_secLep", 0., 1., 1, 0., 1.);
+    GenAllFound = false;
+  }
+
+  vector<Gen> tmp_gen_jets; //gen_jet1, genjet2;
+  for(unsigned int i=2; i<gens.size(); i++){
+    Gen gen = gens.at(i);
+    Gen mother = gens.at(gen.MotherIndex());
+    if(abs( mother.PID() ) == abs( gen_N_PID )){
+      if(abs( gen.PID() ) <= 6){
+        tmp_gen_jets.push_back( gen );
+      }
+    }
+  }
+  if(tmp_gen_jets.size()!=2){
+    cout << "Can't file correct jets : tmp_gen_jets.size() = tmp_gen_jets" << endl;
+    PrintGen(gens);
+    FillHist("GENFIND_size_tmp_gen_jets", tmp_gen_jets.size(), 1., 10, 0., 10.);
+    GenAllFound = false;
+  }
+
+/*
+  //=== 190113_ElectronTriggerTest
+
+  if(gen_SignalLeptonChannel!=0) return;
   vector<Electron> els = GetElectrons("NOCUT", 50., 2.4);
 
   if(els.size()==0) return;
@@ -92,121 +205,13 @@ void HNWRSignalStudy::executeEventFromParameter(AnalyzerParameter param){
   }
 */
 
+  //==========
+  //=== MAIN
+  //==========
+
   //==== # of LSF jets
 
   vector<FatJet> fatjets_LSF = GetFatJets("HNLSF", 200., 2.4);
-  FillHist("FatJet_LSF_Size", fatjets_LSF.size(), 1., 10, 0., 10.);
-
-  //==== Find Gen objects
-  Gen gen_WR, gen_N, gen_priLep, gen_secLep, gen_jet1, gen_jet2;
-  int gen_N_PID = -1;
-  bool gen_SignalLeptonChannel = -1; // 0 : ee, 1 : mm
-  bool GenAllFound = true;
-  bool IsOffShellWR = false;
-
-  for(unsigned int i=2; i<gens.size(); i++){
-    Gen gen = gens.at(i);
-    if(abs( gen.PID() ) == 9900024 || abs( gen.PID() ) == 34){
-
-     //=== pythia sample has strange things. first WR is nan
-     double forchecknan = gen.M();
-     if(forchecknan != forchecknan) continue;
-
-      gen_WR = gen;
-      break;
-    }
-  }
-  if(gen_WR.IsEmpty()){
-    IsOffShellWR = true;
-    cout << "Can't file gen_WR" << endl;
-    PrintGen(gens);
-    FillHist("GENFIND_no_gen_WR", 0., 1., 1, 0., 1.);
-    GenAllFound = false;
-  }
-  FillHist("M_gen_WR", gen_WR.M(), 1., 6000, 0., 6000.);
-
-  for(unsigned int i=2; i<gens.size(); i++){
-    Gen gen = gens.at(i);
-    if(abs( gen.PID() ) == 9900012 || abs( gen.PID() ) == 9900014 ){
-      gen_N_PID = gen.PID();
-      gen_N = gen;
-      if(abs( gen.PID() ) == 9900012) gen_SignalLeptonChannel = 0;
-      else if(abs( gen.PID() ) == 9900014) gen_SignalLeptonChannel = 1;
-      else gen_SignalLeptonChannel = 999;
-      break;
-    }
-  }
-  if(gen_N.IsEmpty()){
-    cout << "Can't file gen_N" << endl;
-    PrintGen(gens);
-    FillHist("GENFIND_no_gen_N", 0., 1., 1, 0., 1.);
-    GenAllFound = false;
-  }
-  FillHist("M_gen_N", gen_N.M(), 1., 6000, 0., 6000.);
-
-
-  if(!IsOffShellWR){
-    for(unsigned int i=2; i<gens.size(); i++){
-      Gen gen = gens.at(i);
-      Gen mother = gens.at(gen.MotherIndex());
-      if(abs( mother.PID() ) == 9900024 || abs( mother.PID() ) == 34 ){
-        if(abs( gen.PID() ) == 11 || abs( gen.PID() ) == 13){
-          gen_priLep = gen;
-          break;
-        }
-      }
-    }
-  }
-  else{
-    for(unsigned int i=2; i<gens.size(); i++){
-      Gen gen = gens.at(i);
-      Gen mother = gens.at(gen.MotherIndex());
-      if(abs( gen.PID() ) == 11 || abs( gen.PID() ) == 13){
-        gen_priLep = gen;
-        break;
-      }
-    }
-  }
-  if(gen_priLep.IsEmpty()){
-    cout << "Can't file gen_priLep" << endl;
-    PrintGen(gens);
-    FillHist("GENFIND_no_gen_priLep", 0., 1., 1, 0., 1.);
-    GenAllFound = false;
-  }
-
-  for(unsigned int i=2; i<gens.size(); i++){
-    Gen gen = gens.at(i);
-    Gen mother = gens.at(gen.MotherIndex());
-    if(abs( mother.PID() ) == abs( gen_N_PID )){
-      if(abs( gen.PID() ) == 11 || abs( gen.PID() ) == 13){
-        gen_secLep = gen;
-        break;
-      }
-    }
-  }
-  if(gen_secLep.IsEmpty()){
-    cout << "Can't file gen_secLep" << endl;
-    PrintGen(gens);
-    FillHist("GENFIND_no_gen_secLep", 0., 1., 1, 0., 1.);
-    GenAllFound = false;
-  }
-
-  vector<Gen> tmp_gen_jets; //gen_jet1, genjet2;
-  for(unsigned int i=2; i<gens.size(); i++){
-    Gen gen = gens.at(i);
-    Gen mother = gens.at(gen.MotherIndex());
-    if(abs( mother.PID() ) == abs( gen_N_PID )){
-      if(abs( gen.PID() ) <= 6){
-        tmp_gen_jets.push_back( gen );
-      }
-    }
-  }
-  if(tmp_gen_jets.size()!=2){
-    cout << "Can't file correct jets : tmp_gen_jets.size() = tmp_gen_jets" << endl;
-    PrintGen(gens);
-    FillHist("GENFIND_size_tmp_gen_jets", tmp_gen_jets.size(), 1., 10, 0., 10.);
-    GenAllFound = false;
-  }
 
   //==== Gen All Found
   if(!GenAllFound) return;
@@ -225,6 +230,9 @@ void HNWRSignalStudy::executeEventFromParameter(AnalyzerParameter param){
   TString GENLepChannel = "";
   vector<Lepton> leps;
   vector<Lepton> Tight_leps;
+  vector<Lepton> NoIso_leps;
+  std::vector<Electron> NoIso_electrons = GetElectrons("HNWRNoIso", 50., 2.5);
+  std::vector<Muon> NoIso_muons = GetMuons("HNWRNoIso", 50., 2.4);
   if(gen_SignalLeptonChannel==0){
     GENLepChannel = "EE";
     vector<Electron> els = GetAllElectrons();
@@ -235,7 +243,13 @@ void HNWRSignalStudy::executeEventFromParameter(AnalyzerParameter param){
       leps.push_back( lep );
       if( els.at(i).PassID("HNWRTight") ){
         Tight_leps.push_back( lep );
-      } 
+      }
+    }
+    for(unsigned int i=0; i<NoIso_electrons.size(); i++){
+      Lepton lep(NoIso_electrons.at(i));
+      if(lep.Pt()<50) continue;
+      if(NoIso_electrons.at(i).PassID("HNWRLoose")) continue;
+      NoIso_leps.push_back( lep );
     }
   }
   else if(gen_SignalLeptonChannel==1){
@@ -250,9 +264,16 @@ void HNWRSignalStudy::executeEventFromParameter(AnalyzerParameter param){
         Tight_leps.push_back( lep );
       } 
     }
+    for(unsigned int i=0; i<NoIso_muons.size(); i++){
+      Lepton lep(NoIso_muons.at(i));
+      if(lep.Pt()<50) continue;
+      if(NoIso_muons.at(i).PassID("HNWRLoose")) continue;
+      NoIso_leps.push_back( lep );
+    }
   }
   JSFillHist(GENLepChannel, "NoCut_"+GENLepChannel, 0., 1., 1, 0., 1.);
   JSFillHist(GENLepChannel, "NoCut__NTightLep_"+GENLepChannel, Tight_leps.size(), 1., 5, 0., 5.);
+  JSFillHist(GENLepChannel, "FatJet_LSF_Size_"+GENLepChannel, fatjets_LSF.size(), 1., 10, 0., 10.);
 
   double dummy_dR = 0.15;
 
@@ -280,8 +301,9 @@ void HNWRSignalStudy::executeEventFromParameter(AnalyzerParameter param){
       Found_lep_mathced_gen_secLep = true;
     }
   }
-  if(!Found_lep_mathced_gen_secLep) return;
-  JSFillHist(GENLepChannel, "GenStudy__lep_mathced_gen_secLep__RelIso_"+GENLepChannel, lep_mathced_gen_secLep.RelIso(), 1., 100, 0., 1.);
+  if(Found_lep_mathced_gen_secLep){
+    JSFillHist(GENLepChannel, "GenStudy__lep_mathced_gen_secLep__RelIso_"+GENLepChannel, lep_mathced_gen_secLep.RelIso(), 1., 100, 0., 1.);
+  }
 
   vector<FatJet> fatjets = GetFatJets("tight", 200., 2.4);
   FatJet fatjet_matched_gen_N;
@@ -302,11 +324,19 @@ void HNWRSignalStudy::executeEventFromParameter(AnalyzerParameter param){
     double reldpf = fabs( fatjet_matched_gen_N.Pt() - gen_N.Pt() )/ gen_N.Pt();
     JSFillHist(GENLepChannel, "GenStudy__fatjet_matched_gen_N__reldpt_"+GENLepChannel, reldpf, 1., 200, 0., 2.);
     JSFillHist(GENLepChannel, "GenStudy__fatjet_matched_gen_N__dRgen_"+GENLepChannel, fatjet_matched_gen_N.DeltaR(gen_N), 1., 60, 0., 6.);
-    JSFillHist(GENLepChannel, "GenStudy__fatjet_matched_gen_N__dR_lep_mathced_gen_secLep_"+GENLepChannel, fatjet_matched_gen_N.DeltaR(lep_mathced_gen_secLep), 1., 60, 0., 6.);
+    if(Found_lep_mathced_gen_secLep) JSFillHist(GENLepChannel, "GenStudy__fatjet_matched_gen_N__dR_lep_mathced_gen_secLep_"+GENLepChannel, fatjet_matched_gen_N.DeltaR(lep_mathced_gen_secLep), 1., 60, 0., 6.);
 
     bool PassUMNTag = (fatjet_matched_gen_N.SDMass() > 40.) && (fatjet_matched_gen_N.LSF() > 0.7);;
     if(PassUMNTag){
       JSFillHist(GENLepChannel, "GenStudy__fatjet_matched_gen_N__PassUMNTag_"+GENLepChannel, 0., 1., 1, 0., 1.);
+
+      //==== Fast vs Full
+
+      JSFillHist(GENLepChannel, "GenStudy__lep_mathced_gen_priLep__Pt_"+GENLepChannel, lep_mathced_gen_priLep.Pt(), 1., 1000, 0., 1000.);
+      if(Found_lep_mathced_gen_secLep){
+        JSFillHist(GENLepChannel, "GenStudy__lep_mathced_gen_secLep__Pt_"+GENLepChannel, lep_mathced_gen_secLep.Pt(), 1., 1000, 0., 1000.);
+      }
+
     }
     else{
       JSFillHist(GENLepChannel, "GenStudy__FailUMNTag__NTightLep_"+GENLepChannel, Tight_leps.size(), 1., 5, 0., 5.);
@@ -314,8 +344,8 @@ void HNWRSignalStudy::executeEventFromParameter(AnalyzerParameter param){
     }
 
     bool PassSNUTag = false;
-    for(unsigned int i=0; i<leps.size(); i++){
-      Lepton lep = leps.at(i);
+    for(unsigned int i=0; i<NoIso_leps.size(); i++){
+      Lepton lep = NoIso_leps.at(i);
       if(lep.Pt()>=53.){
         if( fatjet_matched_gen_N.DeltaR( lep ) < 0.8 ){
           JSFillHist(GENLepChannel, "GenStudy__SNUTagLepton__RelIso_"+GENLepChannel, lep.RelIso(), 1., 100, 0., 1.);
@@ -330,7 +360,6 @@ void HNWRSignalStudy::executeEventFromParameter(AnalyzerParameter param){
     }
 
   } // FatJet Study
-
 
 
 }
