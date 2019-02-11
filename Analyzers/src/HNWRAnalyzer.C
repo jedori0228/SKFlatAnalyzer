6410a11ce2f96e85897fb8d70638c677acf3d2b1
Jae Sung Kim <jae.sung.kim@cern.ch>
2019-02-12 06:20:52 +0900

    hmm test loose lepton tratement

--- Analyzers/src/HNWRAnalyzer.C
+++ Analyzers/src/HNWRAnalyzer.C
@@ -218,32 +218,20 @@ void HNWRAnalyzer::executeEventFromParameter(AnalyzerParameter param){
   std::vector<Electron> Loose_electrons = SelectElectrons(this_AllElectrons, param.Electron_Loose_ID, param.Electron_MinPt, 2.5);
   std::vector<Muon> Loose_muons = SelectMuons(this_AllTunePMuons, param.Muon_Loose_ID, param.Muon_MinPt, 2.4);
 
+  std::vector<Electron> Tight_electrons = SelectElectrons(this_AllElectrons, param.Electron_Tight_ID, param.Electron_MinPt, 2.5);
+  std::vector<Muon> Tight_muons = SelectMuons(this_AllTunePMuons, param.Muon_Tight_ID, param.Muon_MinPt, 2.4);
+
   if(PromptLeptonOnly){
     Loose_electrons = ElectronPromptOnly(Loose_electrons, gens);
     Loose_muons = MuonPromptOnly(Loose_muons, gens);
+    Tight_electrons = ElectronPromptOnly(Tight_electrons, gens);
+    Tight_muons = MuonPromptOnly(Tight_muons, gens);
   }
   //==== Sorting
   std::sort(Loose_electrons.begin(), Loose_electrons.end(), PtComparing);
   std::sort(Loose_muons.begin(), Loose_muons.end(), PtComparing);
-
-  std::vector<Electron> NoIso_electrons = SelectElectrons(this_AllElectrons, "HNWRNoIso", 50., 2.5);
-  std::vector<Muon> NoIso_muons = SelectMuons(this_AllMuons, "HNWRNoIso", 50., 2.4);
-
-  std::vector<Electron> Tight_electrons;
-  std::vector<Muon>     Tight_muons;
-
-  for(unsigned int i=0; i<Loose_electrons.size(); i++){
-
-    //==== Is Tight?
-    if(Loose_electrons.at(i).PassID(param.Electron_Tight_ID)) Tight_electrons.push_back( Loose_electrons.at(i) );
-
-  }
-  for(unsigned int i=0; i<Loose_muons.size(); i++){
-
-    //==== Is Tight?
-    if(Loose_muons.at(i).PassID(param.Muon_Tight_ID)) Tight_muons.push_back( Loose_muons.at(i) );
-
-  }
+  std::sort(Tight_electrons.begin(), Tight_electrons.end(), PtComparing);
+  std::sort(Tight_muons.begin(), Tight_muons.end(), PtComparing);
 
   int n_Veto_leptons = Veto_electrons.size()+Veto_muons.size();
   int n_Loose_leptons = Loose_electrons.size()+Loose_muons.size();
@@ -259,25 +247,6 @@ void HNWRAnalyzer::executeEventFromParameter(AnalyzerParameter param){
   FillHist("n_Loose_leptons_"+param.Name, n_Loose_leptons, 1., 5, 0., 5.);
   FillHist("n_Veto_leptons_"+param.Name, n_Veto_leptons, 1., 5, 0., 5.);
 
-  bool HasCFElectron = false;
-  if(!IsDATA){
-    for(unsigned int i=0; i<Loose_electrons.size(); i++){
-      Gen genlep = GetGenMatchedLepton(Loose_electrons.at(i) , gens);
-      if(genlep.Charge()!=Loose_electrons.at(i).Charge()){
-        HasCFElectron = true;
-        break;
-      }
-    }
-  }
-
-  bool NoExtraLepton = (n_Veto_leptons  == n_Loose_leptons);
-  bool IsAllTight    = (n_Loose_leptons == n_Tight_leptons);
-
-  //==== Veto Extra Lepton
-  if(!NoExtraLepton) return;
-
-  FillHist("NoExtraLepton_"+param.Name, 0., 1., 1, 0., 1.);
-
 /*
   //==== Loose sample or not
   if(RunFake){
@@ -287,8 +256,6 @@ void HNWRAnalyzer::executeEventFromParameter(AnalyzerParameter param){
     if(!IsAllTight) return;
   }
 */
-
-  FillHist("AllTight_"+param.Name, 0., 1., 1, 0., 1.);
 
   FillHist("AfterAllTight_n_Tight_electrons_"+param.Name, Tight_electrons.size(), 1., 5, 0., 5.);
   FillHist("AfterAllTight_n_Loose_electrons_"+param.Name, Loose_electrons.size(), 1., 5, 0., 5.);
@@ -343,9 +310,9 @@ void HNWRAnalyzer::executeEventFromParameter(AnalyzerParameter param){
     //"EMu",
   };
   std::vector< bool > PassTriggers = {
-    PassMu50           && (Loose_electrons.size()==0) && (Loose_muons.size()>=1) && (Tight_muons.size()>=1),
-    PassSingleElectron && (Loose_electrons.size()>=1) && (Loose_muons.size()==0) && (Tight_electrons.size()>=1),
-    //PassMu50           && (Loose_electrons.size()==1) && (Loose_muons.size()==1) && (n_Tight_leptons>=1),
+    PassMu50           && (Tight_electrons.size()==0) && (Tight_muons.size()>=1),
+    PassSingleElectron && (Tight_electrons.size()>=1) && (Tight_muons.size()==0),
+    //PassMu50           && (Tight_electrons.size()==1) && (Tight_muons.size()==1) && (n_Tight_leptons>=1),
   };
 
   //=================
@@ -367,16 +334,16 @@ void HNWRAnalyzer::executeEventFromParameter(AnalyzerParameter param){
     //==== Event Selection : Trigger Safe Pt Selection
 
     if(Suffix.Contains("SingleMuon")){
-      if( Loose_muons.at(0).Pt() < TriggerSafePt_Muon ) continue;
-      if(! (Loose_muons.at(0).PassID(param.Muon_Tight_ID)) ) continue;
+      if( Tight_muons.at(0).Pt() < TriggerSafePt_Muon ) continue;
+      if(! (Tight_muons.at(0).PassID(param.Muon_Tight_ID)) ) continue;
     }
     else if(Suffix.Contains("SingleElectron")){
-      if( Loose_electrons.at(0).Pt() < TriggerSafePt_Electron ) continue;
-      if(! (Loose_electrons.at(0).PassID(param.Electron_Tight_ID)) ) continue;
+      if( Tight_electrons.at(0).Pt() < TriggerSafePt_Electron ) continue;
+      if(! (Tight_electrons.at(0).PassID(param.Electron_Tight_ID)) ) continue;
     }
     else if(Suffix.Contains("EMu")){
-      if( Loose_muons.at(0).Pt() < TriggerSafePt_Muon ) continue;
-      if( Loose_electrons.at(0).Pt() < TriggerSafePt_Electron ) continue;
+      if( Tight_muons.at(0).Pt() < TriggerSafePt_Muon ) continue;
+      if( Tight_electrons.at(0).Pt() < TriggerSafePt_Electron ) continue;
     }
     else{
 
@@ -399,32 +366,19 @@ void HNWRAnalyzer::executeEventFromParameter(AnalyzerParameter param){
       }
     }
 
-    std::vector<Lepton *> leps_el, leps_mu;
-    std::vector<Lepton *> leps;
-    leps_el = MakeLeptonPointerVector(Loose_electrons);
-    leps_mu = MakeLeptonPointerVector(Loose_muons);
-    for(unsigned int i=0; i<leps_el.size(); i++) leps.push_back( leps_el.at(i) );
-    for(unsigned int i=0; i<leps_mu.size(); i++) leps.push_back( leps_mu.at(i) );
+    std::vector<Lepton *> Tight_leps_el, Tight_leps_mu;
+    std::vector<Lepton *> Tight_leps;
+    Tight_leps_el = MakeLeptonPointerVector(Tight_electrons);
+    Tight_leps_mu = MakeLeptonPointerVector(Tight_muons);
+    for(unsigned int i=0; i<Tight_leps_el.size(); i++) Tight_leps.push_back( Tight_leps_el.at(i) );
+    for(unsigned int i=0; i<Tight_leps_mu.size(); i++) Tight_leps.push_back( Tight_leps_mu.at(i) );
 
-    std::vector<Lepton *> leps_NoIso_SF, leps_NoIso_OF;
-    if(Suffix.Contains("SingleMuon")){
-      leps_NoIso_SF = MakeLeptonPointerVector(NoIso_muons);
-      leps_NoIso_OF = MakeLeptonPointerVector(NoIso_electrons);
-    }
-    else if(Suffix.Contains("SingleElectron")){
-      leps_NoIso_SF = MakeLeptonPointerVector(NoIso_electrons);
-      leps_NoIso_OF = MakeLeptonPointerVector(NoIso_muons);
-    }
-    else if(Suffix.Contains("EMu")){
-
-      //==== Always dilepton event. No need this.
-      //==== Sort leps here
-      std::sort( leps.begin(), leps.end(), PtComparingPtr);
-
-    }
-    else{
-
-    }
+    std::vector<Lepton *> Loose_leps_el, Loose_leps_mu;
+    std::vector<Lepton *> Loose_leps;
+    Loose_leps_el = MakeLeptonPointerVector(Loose_electrons);
+    Loose_leps_mu = MakeLeptonPointerVector(Loose_muons);
+    for(unsigned int i=0; i<Loose_leps_el.size(); i++) Loose_leps.push_back( Loose_leps_el.at(i) );
+    for(unsigned int i=0; i<Loose_leps_mu.size(); i++) Loose_leps.push_back( Loose_leps_mu.at(i) );
 
     double weight = 1.;
     if(!IsDATA){
@@ -432,47 +386,32 @@ void HNWRAnalyzer::executeEventFromParameter(AnalyzerParameter param){
 
       mcCorr->IgnoreNoHist = param.MCCorrrectionIgnoreNoHist;
 
-      for(unsigned int i=0; i<Loose_electrons.size(); i++){
-        double this_recosf = mcCorr->ElectronReco_SF(Loose_electrons.at(i).scEta(),Loose_electrons.at(i).Pt());
-        double this_idsf = mcCorr->ElectronID_SF(param.Electron_ID_SF_Key, Loose_electrons.at(i).scEta(), Loose_electrons.at(i).Pt());
+      for(unsigned int i=0; i<Tight_electrons.size(); i++){
+        double this_recosf = mcCorr->ElectronReco_SF(Tight_electrons.at(i).scEta(),Tight_electrons.at(i).Pt());
+        double this_idsf = mcCorr->ElectronID_SF(param.Electron_ID_SF_Key, Tight_electrons.at(i).scEta(), Tight_electrons.at(i).Pt());
         weight *= this_recosf*this_idsf;
       }
-      for(unsigned int i=0; i<Loose_muons.size(); i++){
-        double this_idsf  = mcCorr->MuonID_SF (param.Muon_ID_SF_Key,  Loose_muons.at(i).Eta(), Loose_muons.at(i).MiniAODPt());
-        double this_isosf = mcCorr->MuonISO_SF(param.Muon_ISO_SF_Key, Loose_muons.at(i).Eta(), Loose_muons.at(i).MiniAODPt());
+      for(unsigned int i=0; i<Tight_muons.size(); i++){
+        double this_idsf  = mcCorr->MuonID_SF (param.Muon_ID_SF_Key,  Tight_muons.at(i).Eta(), Tight_muons.at(i).MiniAODPt());
+        double this_isosf = mcCorr->MuonISO_SF(param.Muon_ISO_SF_Key, Tight_muons.at(i).Eta(), Tight_muons.at(i).MiniAODPt());
         weight *= this_idsf*this_isosf;
       }
 
       double this_trigsf = 1.;
       if(Suffix.Contains("SingleMuon")){
-        this_trigsf = mcCorr->MuonTrigger_SF(param.Muon_Trigger_SF_Key, TriggerNameForSF_Muon, Loose_muons);
+        this_trigsf = mcCorr->MuonTrigger_SF(param.Muon_Trigger_SF_Key, TriggerNameForSF_Muon, Tight_muons);
       }
       else if(Suffix.Contains("SingleElectron")){
         this_trigsf = 1.;
       }
       else if(Suffix.Contains("EMu")){
-        this_trigsf = mcCorr->MuonTrigger_SF(param.Muon_Trigger_SF_Key, TriggerNameForSF_Muon, Loose_muons);
+        this_trigsf = mcCorr->MuonTrigger_SF(param.Muon_Trigger_SF_Key, TriggerNameForSF_Muon, Tight_muons);
       }
       else{
 
       }
 
       weight *= this_trigsf;
-
-    }
-    else{
-
-      if(RunFake){
-        weight = fakeEst->GetWeight(leps, param);
-        if(! (fakeEst->HasLooseLepton) ){
-          cout << "--> WTF" << endl;
-          exit(EXIT_FAILURE);
-        }
-      }
-
-      if(RunCF){
-        weight = cfEst->GetWeight(leps, param);
-      }
 
     }
 
@@ -486,126 +425,19 @@ void HNWRAnalyzer::executeEventFromParameter(AnalyzerParameter param){
     Particle NCand;
     Particle NCand_1, NCand_2;
 
-    FillHist(Suffix+"_NLepton_"+param.Name, leps.size(), 1., 5, 0., 5.);
+    FillHist(Suffix+"_NTightLepton_"+param.Name, Tight_leps.size(), 1., 5, 0., 5.);
+    FillHist(Suffix+"_NLooseLepton_"+param.Name, Loose_leps.size(), 1., 5, 0., 5.);
 
-/*
-    //==== If only one lepton, it should be boosted
-    if(leps.size()==1){
-
-      bool FoundAwayFatJet = false;
-      bool FoundAwayFatJetWithSFNonIsoLepton = false;
-      bool FoundAwayFatJetWithOFNonIsoLepton = false;
-
-      if(fatjets.size()>=1){
-
-        for(unsigned int i=0; i<fatjets.size(); i++){
-
-          if( fabs(leps[0]->DeltaPhi( fatjets.at(i) )) > 2.0 ){
-            HNFatJet = fatjets.at(i);
-            FoundAwayFatJet = true;
-
-            //==== 1) NoIso lepton should be inside AK8 jet
-            for(unsigned int j=0; j<leps_NoIso_SF.size(); j++){
-              if(HNFatJet.DeltaR( *(leps_NoIso_SF.at(j)) ) < 0.8){
-                FoundAwayFatJetWithSFNonIsoLepton = true;
-                break;
-              }
-            }
-            //==== 2) NoIso_Veto lepton should NOT be inside AK8 jet
-            for(unsigned int j=0; j<leps_NoIso_OF.size(); j++){
-              if(HNFatJet.DeltaR( *(leps_NoIso_OF.at(j)) ) < 0.8){
-                FoundAwayFatJetWithOFNonIsoLepton = true;
-                break;
-              }
-            }
-
-            break;
-          }
-
-        }
-
-      }
-
-      if(FoundAwayFatJet){
-        map_bool_To_Region["OneLepton_AwayFatJet"] = true;
-        WRCand = (*leps[0])+HNFatJet;
-        NCand = HNFatJet;
-        NCand_1 = HNFatJet;
-        NCand_2 = HNFatJet;
-
-        //==== SR : AwayJet has SF Noniso lepton inside
-        //====           but no OF Noniso lepton inside
-        if(FoundAwayFatJetWithSFNonIsoLepton && !FoundAwayFatJetWithOFNonIsoLepton){
-          map_bool_To_Region["OneLepton_AwayFatJetWithSFLepton"] = true;
-          if(leps[0]->Pt() > 100.){
-            map_bool_To_Region["OneLepton_AwayFatJetWithSFLepton100GeV"] = true;
-          }
-        }
-        //==== CR : AwayJet has OF Noniso lepton inside
-        //====           but no SF Noniso lepton inside
-        if(!FoundAwayFatJetWithSFNonIsoLepton && FoundAwayFatJetWithOFNonIsoLepton){
-          map_bool_To_Region["OneLepton_AwayFatJetWithOFLepton"] = true;
-          if(leps[0]->Pt() > 100.){
-            map_bool_To_Region["OneLepton_AwayFatJetWithOFLepton100GeV"] = true;
-          }
-        }
-
-      } // END if FoundAwayFatJet
-
-    }
-    else if(leps.size()==2){
-
-      double m_ll = ( (*leps[0])+(*leps[1]) ).M();
-
-      if(m_ll>15.){
-
-        Particle Jets;
-        bool IsJetFound;
-
-        if(jets.size()>=2){
-          TMP_map_bool_To_Region["TwoLepton_TwoJet"] = true;
-          WRCand = (*leps[0])+(*leps[1])+jets.at(0)+jets.at(1);
-          NCand = (*leps[1])+jets.at(0)+jets.at(1);
-          NCand_1 = (*leps[0])+jets.at(0)+jets.at(1);
-          NCand_2 = (*leps[1])+jets.at(0)+jets.at(1);
-
-          if(m_ll>150.){
-            TMP_map_bool_To_Region["TwoLepton_TwoJet_mllgt150"] = true;
-          }
-          else{
-            TMP_map_bool_To_Region["TwoLepton_TwoJet_mlllt150"] = true;
-          }
-
-        }
-
-      }
-
-    }
-
-    //==== For TwoLeptons
-    for(std::map<TString, bool>::iterator it_map = TMP_map_bool_To_Region.begin(); it_map != TMP_map_bool_To_Region.end(); it_map++){
-      TString this_region = it_map->first;
-
-      bool IsOS = (leps[0]->Charge() != leps[1]->Charge());
-      if(RunCF) IsOS = !IsOS;
-
-      if(it_map->second){
-        map_bool_To_Region[this_region] = true;
-        if(IsOS) map_bool_To_Region[this_region+"_OS"] = true;
-        else     map_bool_To_Region[this_region+"_SS"] = true;
-      }
-
-    }
-*/
+    vector<Lepton *> Used_leps;
 
     //==== UMN
 
-    Lepton LeadLep = (*leps[0]);
+    Lepton LeadLep = (*Tight_leps.at(0));
     bool LeadLepPtCut = (LeadLep.Pt() > 60.);
     if(!LeadLepPtCut) continue;
 
-    if(leps.size()==2 && IsAllTight){
-      Lepton SubLeadLep = (*leps[1]);
+    if(Tight_leps.size()==2){
+      Lepton SubLeadLep = (*Tight_leps[1]);
       bool SubLeadLepPtCut = (SubLeadLep.Pt() > 53.);
       bool IsResolved = SubLeadLepPtCut && ( (LeadLep+SubLeadLep).M() >= 200. ) && (jets.size() >= 2);
       IsResolved = IsResolved && (LeadLep.DeltaR( SubLeadLep ) > 0.4);
@@ -616,6 +448,8 @@ void HNWRAnalyzer::executeEventFromParameter(AnalyzerParameter param){
         NCand = SubLeadLep+jets.at(0)+jets.at(1);
         NCand_1 = LeadLep+jets.at(0)+jets.at(1);
         NCand_2 = SubLeadLep+jets.at(0)+jets.at(1);
+        Used_leps.push_back( Tight_leps.at(0) );
+        Used_leps.push_back( Tight_leps.at(1) );
         if(fatjets_LSF.size()==0){
           map_bool_To_Region["Resolved_NoLSFFatJet"] = true;
         }
@@ -639,12 +473,15 @@ void HNWRAnalyzer::executeEventFromParameter(AnalyzerParameter param){
       }
       if(IsBoosted){
         map_bool_To_Region["Boosted"] = true;
-        if(leps.size()>=2){
+        Used_leps.push_back( Tight_leps.at(0) );
+        if(Loose_leps.size()>=2){
 
-          for(unsigned int k=1; k<leps.size(); k++){
-            if( leps.at(k)->Pt() <= 53. ) continue;
-            if( HNFatJet.DeltaR( *(leps.at(k)) ) < 0.8 ){
+          for(unsigned int k=0; k<Loose_leps.size(); k++){
+            if( Loose_leps.at(k)->Pt() <= 53. ) continue;
+            if( HNFatJet.DeltaR( *(Loose_leps.at(k)) ) < 0.8 ){
               map_bool_To_Region["Boosted_TwoLepton"] = true;
+              Used_leps.push_back( Loose_leps.at(k) );
+              break;
             }
           }
 
@@ -669,14 +506,14 @@ void HNWRAnalyzer::executeEventFromParameter(AnalyzerParameter param){
         JSFillHist(this_region, "nPileUp_"+this_region, nPileUp, weight, 200., 0., 200.);
         JSFillHist(this_region, "nPV_"+this_region, nPV, weight, 200., 0., 200.);
 
-        JSFillHist(this_region, "Lepton_Size_"+this_region, leps.size(), weight, 10, 0., 10.);
+        JSFillHist(this_region, "Lepton_Size_"+this_region, Used_leps.size(), weight, 10, 0., 10.);
         JSFillHist(this_region, "FatJet_Size_"+this_region, fatjets.size(), weight, 10, 0., 10.);
         JSFillHist(this_region, "LSFFatJet_Size_"+this_region, fatjets_LSF.size(), weight, 10, 0., 10.);
         JSFillHist(this_region, "FatJet_LSF_Size_"+this_region, fatjets_LSF.size(), weight, 10, 0., 10.);
         JSFillHist(this_region, "Jet_Size_"+this_region, jets.size(), weight, 10, 0., 10.);
 
         if(this_region.Contains("Boosted")){
-          JSFillHist(this_region, "dPhi_lJ_"+this_region, fabs( leps[0]->DeltaPhi(HNFatJet) ), weight, 40, 0., 4.);
+          JSFillHist(this_region, "dPhi_lJ_"+this_region, fabs( Used_leps.at(0)->DeltaPhi(HNFatJet) ), weight, 40, 0., 4.);
 
           JSFillHist(this_region, "HNFatJet_Pt_"+this_region, HNFatJet.Pt(), weight, 1000, 0., 1000.);
           JSFillHist(this_region, "HNFatJet_Eta_"+this_region, HNFatJet.Eta(), weight, 60, -3., 3.);
@@ -690,9 +527,9 @@ void HNWRAnalyzer::executeEventFromParameter(AnalyzerParameter param){
         }
 
         if(this_region.Contains("TwoLepton") || this_region.Contains("Resolved")){
-          JSFillHist(this_region, "ZCand_Mass_"+this_region, ((*leps[0])+(*leps[1])).M(), weight, 2000, 0., 2000.);
-          JSFillHist(this_region, "ZCand_Pt_"+this_region, ((*leps[0])+(*leps[1])).Pt(), weight, 2000, 0., 2000.);
-          JSFillHist(this_region, "dPhi_ll_"+this_region, fabs((*leps[0]).DeltaPhi(*leps[1])), weight, 40, 0., 4.);
+          JSFillHist(this_region, "ZCand_Mass_"+this_region, ((*Used_leps.at(0))+(*Used_leps.at(1))).M(), weight, 2000, 0., 2000.);
+          JSFillHist(this_region, "ZCand_Pt_"+this_region, ((*Used_leps.at(0))+(*Used_leps.at(1))).Pt(), weight, 2000, 0., 2000.);
+          JSFillHist(this_region, "dPhi_ll_"+this_region, fabs((*Used_leps.at(0)).DeltaPhi(*Used_leps.at(1))), weight, 40, 0., 4.);
         }
 
         JSFillHist(this_region, "WRCand_Mass_"+this_region, WRCand.M(), weight, 800, 0., 8000.);
@@ -706,7 +543,7 @@ void HNWRAnalyzer::executeEventFromParameter(AnalyzerParameter param){
         JSFillHist(this_region, "NCand_2_Pt_"+this_region, NCand_2.Pt(), weight, 300, 0., 3000.);
 
 
-        FillLeptonPlots(leps, this_region, weight);
+        FillLeptonPlots(Used_leps, this_region, weight);
         FillJetPlots(jets, fatjets, this_region, weight);
 
       } // END if(pass Region)
