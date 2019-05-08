2c564d843964235ea6c5fffe2adee52cead1131b
Jae Sung Kim <jae.sung.kim@cern.ch>
2019-05-09 01:15:38 +0900

    Check DoubleTrigger skimming

--- Analyzers/src/MyPlayGround.C
+++ Analyzers/src/MyPlayGround.C
@@ -32,7 +32,7 @@ std::vector<Lepton *> MyPlayGround::TESTFunction(std::vector<Muon>& muons){
 }
 
 void MyPlayGround::executeEventFromParameter(AnalyzerParameter param){
-
+/*
   vector<Gen> gens = GetGens();
   Particle GenZ = genFinderDY->Find(gens);
   FillHist("Unweighted_GenZ_Mass" , GenZ.M(), 1., 5000, 0., 5000.);
@@ -50,6 +50,36 @@ void MyPlayGround::executeEventFromParameter(AnalyzerParameter param){
   FillHist("Method_"+str_MethodUsed+"_Unweighted_GenZ_Pt" , GenZ.Pt(), 1., 3000, 0., 3000.);
   FillHist("Method_"+str_MethodUsed+"_Weighted_GenZ_Mass" , GenZ.M(), weight, 5000, 0., 5000.);
   FillHist("Method_"+str_MethodUsed+"_Weighted_GenZ_Pt" , GenZ.Pt(), weight, 3000, 0., 3000.);
+*/
+
+  vector<TString> triggers_Default = {
+    "HLT_Mu50_v",
+    "HLT_TkMu50_v",
+    "HLT_Ele27_WPTight_Gsf_v",
+    "HLT_Photon175_v",
+    "HLT_DoublePhoton60_v",
+  };
+  vector<TString> triggers_ToAdd = {
+    "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v",
+    "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v",
+    "HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v",
+    "HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v",
+    "HLT_TkMu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v",
+    "HLT_TkMu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v",
+    "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
+  };
+
+  Event ev = GetEvent();
+  bool Pass_Default = ev.PassTrigger(triggers_Default);
+  bool Pass_ToAdd = ev.PassTrigger(triggers_ToAdd);
+
+  if(Pass_Default){
+    FillHist("LRSMHighPt", 0., 1., 1, 0., 1.);
+  }
+  if(Pass_Default||Pass_ToAdd){
+    FillHist("LRSMHighPt_PlusDouble", 0., 1., 1, 0., 1.);
+  }
+
 
 }
 
